e40309f591647e8caa6fbd4c0ed0806c1ad09f91
WSL2 Ubuntu Furuya <inf.4clover@gmail.com>
2024-06-30 12:06:21 +0900

    first commit

---
+++ src/tests/SampleTest.php
@@ -0,0 +1,15 @@
+<?php
+use \PHPUnit\Framework\TestCase;
+
+require_once(dirname(__FILE__) . "/../sample.php");
+
+class TestSample extends TestCase
+{
+	public function testReturnHelloWorld()
+	{
+		$sample = new Sample();
+		$result = $sample->helloWorld();
+
+		$this->assertEquals("hello world", $result);
+	}
+}
